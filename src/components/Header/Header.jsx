21b0b1f4f3e9abdac210540afdd73c4e3b48c52d
Flame <5017_30@mail.ru>
2023-06-09 10:55:52 +0300

    Updates

--- src/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Logo from '../../assets/logo.png'
-import s from './Header.module.scss'
-
-const Header = () => {
-	return <img className={s.logo} src={Logo} alt='Логотип' />
-}
-
-export default Header
